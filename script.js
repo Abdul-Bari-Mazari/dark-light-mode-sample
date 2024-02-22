15b0f6a1aa40e2401599aa1443e48192a1dc9da7
Abdul-Bari-Mazari <mazari3339@gmail.com>
2024-02-22 16:03:27 +0500

    Code Cleanup to "dark-light-mode-sample)

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-const themeToggle = document.getElementById('themeToggle');
+const themeToggle = document.getElementById("themeToggle");
 const body = document.body;
-const header = document.querySelector('header');
-const content = document.querySelector('.content');
 
-themeToggle.addEventListener('change', function() {
-    if(this.checked) {
-        body.setAttribute('data-theme', 'dark');
-        header.setAttribute('data-theme', 'dark');
-        content.setAttribute('data-theme', 'dark');
-    } else {
-        body.setAttribute('data-theme', 'light');
-        header.setAttribute('data-theme', 'light');
-        content.setAttribute('data-theme', 'light');
-    }
-});
+function changeTheme() {
+  if (themeToggle.checked) {
+    body.setAttribute("data-theme", "dark");
+  } else {
+    body.setAttribute("data-theme", "light");
+  }
+}
+
+// const themeToggle = document.getElementById("themeToggle");
+// const body = document.body;
+// const header = document.querySelector("header");
+// const content = document.querySelector(".content");
+
+// function changeTheme() {
+//   if (themeToggle.checked) {
+//     body.setAttribute("data-theme", "dark");
+//     header.setAttribute("data-theme", "dark");
+//     content.setAttribute("data-theme", "dark");
+//   } else {
+//     body.setAttribute("data-theme", "light");
+//     header.setAttribute("data-theme", "light");
+//     content.setAttribute("data-theme", "light");
+//   }
+// }
